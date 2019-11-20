96751cd314afbca32aeaf3641857526bfe5a1bfe
Alex Graur <Alex.Graur@endava.com>
2019-11-20 15:23:04 +0200

    added reader related commands and messages

---
+++ SmartCredentials_aOS/eid/src/main/java/de/telekom/smartcredentials/eid/messages/ReaderMessage.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2019 Telekom Deutschland AG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.telekom.smartcredentials.eid.messages;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import de.telekom.smartcredentials.core.eid.messages.EidMessage;
+
+/**
+ * Created by Alex.Graur@endava.com at 11/19/2019
+ */
+@SuppressWarnings("unused")
+public class ReaderMessage extends EidMessage {
+
+    @SerializedName("name")
+    @Expose
+    private String mName;
+    @SerializedName("attached")
+    @Expose
+    private boolean mAttached;
+    @SerializedName("keypad")
+    @Expose
+    private boolean mKeypad;
+    @SerializedName("card")
+    @Expose
+    private Card mCard;
+
+    public ReaderMessage() {
+
+    }
+
+    @Override
+    public String getName() {
+        return mName;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.mName = name;
+    }
+
+    public boolean isAttached() {
+        return mAttached;
+    }
+
+    public void setAttached(boolean attached) {
+        this.mAttached = attached;
+    }
+
+    public boolean isKeypad() {
+        return mKeypad;
+    }
+
+    public void setKeypad(boolean keypad) {
+        this.mKeypad = keypad;
+    }
+
+    public Card getCard() {
+        return mCard;
+    }
+
+    public void setCard(Card card) {
+        this.mCard = card;
+    }
+
+    @Override
+    public String toString() {
+        return "ReaderMessage{" +
+                "mName='" + mName + '\'' +
+                ", mAttached=" + mAttached +
+                ", mKeypad=" + mKeypad +
+                ", mCard=" + mCard +
+                '}';
+    }
+}
