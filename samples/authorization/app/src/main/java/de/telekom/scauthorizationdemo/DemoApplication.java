5d4ab56337b6751f27a9a208a1c3691c1fbf7e94
Gabriel Blaj <gabriel.blaj@endava.com>
2020-09-03 11:40:32 +0300

    created app and added sc modules

---
+++ samples/authorization/app/src/main/java/de/telekom/scauthorizationdemo/DemoApplication.java
@@ -0,0 +1,41 @@
+package de.telekom.scauthorizationdemo;
+
+import android.app.Application;
+
+import de.telekom.smartcredentials.authorization.factory.SmartCredentialsAuthorizationFactory;
+import de.telekom.smartcredentials.core.api.CoreApi;
+import de.telekom.smartcredentials.core.configurations.SmartCredentialsConfiguration;
+import de.telekom.smartcredentials.core.factory.SmartCredentialsCoreFactory;
+import de.telekom.smartcredentials.core.rootdetector.RootDetectionOption;
+import de.telekom.smartcredentials.security.factory.SmartCredentialsSecurityFactory;
+import de.telekom.smartcredentials.storage.factory.SmartCredentialsStorageFactory;
+import timber.log.Timber;
+
+/**
+ * Created by gabriel.blaj@endava.com at 9/3/2020
+ */
+public class DemoApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Timber.plant(new Timber.DebugTree());
+        initSmartCredentialsModules();
+    }
+
+    private void initSmartCredentialsModules() {
+        SmartCredentialsConfiguration configuration = new SmartCredentialsConfiguration.Builder(getApplicationContext(), getString(R.string.current_user_id))
+                .setLogger(new DemoLogger())
+                .setRootCheckerEnabled(RootDetectionOption.ALL)
+                .setAppAlias(getString(R.string.app_alias))
+                .build();
+        SmartCredentialsCoreFactory.initialize(configuration);
+        CoreApi coreApi = SmartCredentialsCoreFactory.getSmartCredentialsCoreApi();
+        SmartCredentialsSecurityFactory.initSmartCredentialsSecurityModule(this, coreApi);
+        SmartCredentialsStorageFactory.initSmartCredentialsStorageModule(this, coreApi,
+                SmartCredentialsSecurityFactory.getSecurityApi());
+        SmartCredentialsAuthorizationFactory.initSmartCredentialsAuthorizationModule(this,
+                coreApi, SmartCredentialsSecurityFactory.getSecurityApi(),
+                SmartCredentialsStorageFactory.getStorageApi());
+    }
+}
---
+++ samples/authorization/app/src/main/java/de/telekom/scauthorizationdemo/DemoLogger.java
@@ -0,0 +1,44 @@
+package de.telekom.scauthorizationdemo;
+
+import de.telekom.smartcredentials.core.logger.ApiLogger;
+
+/**
+ * Created by gabriel.blaj@endava.com at 9/3/2020
+ */
+public class DemoLogger implements ApiLogger {
+
+    @Override
+    public void logError(String className, String errorMessage) {
+
+    }
+
+    @Override
+    public void logMethodAccess(String className, String methodName) {
+
+    }
+
+    @Override
+    public void logCallbackMethod(String className, String callbackClassName, String methodName) {
+
+    }
+
+    @Override
+    public void logFactoryMethod(String factoryClassName, String factoryMethodName, String instantiatedClassName) {
+
+    }
+
+    @Override
+    public void logEvent(String eventMessage) {
+
+    }
+
+    @Override
+    public void logInfo(String infoMessage) {
+
+    }
+
+    @Override
+    public void logExternal(String eventType, String eventName, String eventContent) {
+
+    }
+}
