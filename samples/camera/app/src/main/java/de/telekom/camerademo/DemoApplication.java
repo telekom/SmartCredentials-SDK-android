b4402fea17dc095b15b831ed0c23408e832dd0a1
Alex Graur <Alex.Graur@endava.com>
2020-09-09 15:35:28 +0300

    add logs to DemoLogger

--- samples/camera/app/src/main/java/de/telekom/camerademo/DemoApplication.java
+++ samples/camera/app/src/main/java/de/telekom/camerademo/DemoApplication.java
@@ -7,15 +7,19 @@ import de.telekom.smartcredentials.core.api.CoreApi;
 import de.telekom.smartcredentials.core.configurations.SmartCredentialsConfiguration;
 import de.telekom.smartcredentials.core.factory.SmartCredentialsCoreFactory;
 import de.telekom.smartcredentials.core.rootdetector.RootDetectionOption;
+import timber.log.Timber;
 
 /**
  * Created by Alex.Graur@endava.com at 9/1/2020
  */
 public class DemoApplication extends Application {
 
+    public static final String TAG = "camera_tag";
+
     @Override
     public void onCreate() {
         super.onCreate();
+        Timber.plant(new Timber.DebugTree());
         SmartCredentialsConfiguration configuration = new SmartCredentialsConfiguration.Builder(getApplicationContext(), getString(R.string.user_id))
                 .setLogger(new DemoLogger())
                 .setRootCheckerEnabled(RootDetectionOption.ALL)
