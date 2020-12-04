1d89518deef3fa90404a89307085af8522974a09
Gabriel Blaj <gabriel.blaj@endava.com>
2020-12-04 13:24:33 +0200

    updated document scanner demo app

--- samples/documentscanner/app/src/main/java/de/telekom/scdocumentscannerdemo/recognizer/RecognizerType.java
+++ samples/documentscanner/app/src/main/java/de/telekom/scdocumentscannerdemo/recognizer/RecognizerType.java
@@ -7,14 +7,8 @@ public enum RecognizerType {
 
     ID_COMBINED_RECOGNIZER("ID Combined Recognizer"),
     ID_SIMPLE_RECOGNIZER("ID Simple Recognizer"),
-    DOCUMENT_FACE_RECOGNIZER("Document Face Recognizer"),
     ID_BARCODE_RECOGNIZER("ID Barcode Recognizer"),
-    MRTD_COMBINED_RECOGNIZER("MRTD Combined Recognizer"),
-    MRTD_SIMPLE_RECOGNIZER("MRTD Simple Recognizer"),
-    PASSPORT_RECOGNIZER("Passport Recognizer"),
-    USDL_COMBINED_RECOGNIZER("USDL Combined Recognizer"),
-    USDL_SIMPLE_RECOGNIZER("USDL Simple Recognizer"),
-    VISA_RECOGNIZER("Visa Recognizer");
+    USDL_COMBINED_RECOGNIZER("USDL Combined Recognizer");
 
     private final String desc;
 
