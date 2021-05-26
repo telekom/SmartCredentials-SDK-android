fdc1e454f5ea6ae9045c013a36b07fe60a64936a
Gabriel Blaj <gabriel.blaj@endava.com>
2021-05-26 13:33:42 +0300

    Support for Sim and Entitlements modules

---
+++ SmartCredentials_aOS/core/src/main/java/de/telekom/smartcredentials/core/api/EntitlementsApi.java
@@ -0,0 +1,12 @@
+package de.telekom.smartcredentials.core.api;
+
+import de.telekom.smartcredentials.core.responses.SmartCredentialsApiResponse;
+
+/**
+ * Created by Gabriel.Blaj@endava.com at 5/26/2021
+ */
+@SuppressWarnings("unused")
+public interface EntitlementsApi {
+
+    SmartCredentialsApiResponse<Void> placeholderMethod();
+}
