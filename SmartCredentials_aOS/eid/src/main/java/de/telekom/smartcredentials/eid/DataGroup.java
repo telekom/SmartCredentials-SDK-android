5c5473b19e4d4af0e66688a5a32c5345385aeb5c
Axel Nennker <axel.nennker@telekom.de>
2022-09-04 22:39:32 +0200

    higher level methods and some tests
    Signed-off-by: Axel Nennker <axel.nennker@telekom.de>

---
+++ SmartCredentials_aOS/eid/src/main/java/de/telekom/smartcredentials/eid/DataGroup.java
@@ -0,0 +1,55 @@
+package de.telekom.smartcredentials.eid;
+
+public enum DataGroup {
+    DG1("DG1", "Document Type", "0x0101", "0x01", "DocumentType"),
+    DG2("DG2", "Issuing State, Region and Municipality", "0x0101", "0x01", "IssuingEntity"),
+    DG3("DG3", "Date of Expiry", "0x0101", "0x01", "Date"),
+    DG4("DG4", "Given Names", "0x0101", "0x01", "GivenNames"),
+    DG5("DG5", "Family Names ", "0x0101", "0x01", "FamilyNames"),
+    DG6("DG5", "Nom de Plume", "0x0101", "0x01", "NomDePlume"),
+    DG7("DG7", "Academic Title", "0x0101", "0x01", "AcademicTitle"),
+    DG8("DG8", "Date of Birth", "0x0101", "0x01", "DateOfBirth"),
+    DG9("DG9", "Place of Birth", "0x0101", "0x01", "PlaceOfBirth"),
+    DG10("DG10", "Nationality", "0x0101", "0x01", "Nationality"),
+    DG11("DG11", "Sex", "0x0101", "0x01", "Sex"),
+    DG13("DG13", "Birth Name ", "0x0101", "0x01", "BirthName"),
+    DG15("DG15", "Date of Issuance", "0x0101", "0x01", "Date"),
+    DG17("DG17", "Normal Place of Residence (multiple)", "0x0101", "0x01", "PlaceOfResidence"),
+    DG18("DG18", "Municipality ID ", "0x0101", "0x01", "MunicipalityID"),
+    DG19("DG19", "Residence Permit I", "0x0101", "0x01", "ResidencePermitI"),
+    DG20("DG20", "Residence Permit II", "0x0101", "0x01", "ResidencePermitII");
+
+    private final String mDg;
+    private final String mContent;
+    private final String mFileId;
+    private final String mShortFileId;
+    private final String mASN1Type;
+
+    DataGroup(String dg, String content, String fileId, String shortFileId, String ASN1Type) {
+        mDg = dg;
+        mContent = content;
+        mFileId = fileId;
+        mShortFileId = shortFileId;
+        mASN1Type = ASN1Type;
+    }
+
+    public String getDg() {
+        return mDg;
+    }
+
+    public String getFileId() {
+        return mFileId;
+    }
+
+    public String getShortFileId() {
+        return mShortFileId;
+    }
+
+    public String getASN1Type() {
+        return mASN1Type;
+    }
+
+    public String getContent() {
+        return mContent;
+    }
+}
--- SmartCredentials_aOS/eid/src/main/java/de/telekom/smartcredentials/eid/commands/SetCardCommand.java
+++ SmartCredentials_aOS/eid/src/main/java/de/telekom/smartcredentials/eid/commands/SetCardCommand.java
@@ -21,6 +21,14 @@ import androidx.annotation.NonNull;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.Arrays;
+import java.util.Locale;
+
+import de.persosim.simulator.tlv.Asn1DocumentType;
+import de.persosim.simulator.tlv.Asn1Utf8StringWrapper;
+import de.persosim.simulator.tlv.ConstructedTlvDataObject;
+import de.persosim.simulator.tlv.TlvTag;
+import de.persosim.simulator.utils.HexString;
 import de.telekom.smartcredentials.eid.commands.types.EidCommandType;
 
 /**
@@ -60,7 +68,7 @@ public class SetCardCommand extends SmartEidCommand {
     static class Simulator {
         @SerializedName("files")
         @Expose
-        private final SimulatorFile[] mFiles;
+        private SimulatorFile[] mFiles;
         public Simulator(SimulatorFile[] files) {
             mFiles = files;
         }
@@ -68,6 +76,72 @@ public class SetCardCommand extends SmartEidCommand {
         public SimulatorFile[] getFiles() {
             return mFiles;
         }
+
+        void setFile(@NonNull String fileId, @NonNull String shortFileId, @NonNull String content) {
+            boolean found = false;
+            for (SimulatorFile simulatorFile : mFiles) {
+                if (fileId.equals(simulatorFile.mFileId) && shortFileId.equals(simulatorFile.mShortFileId)) {
+                    simulatorFile.mContent = content;
+                    found = true;
+                }
+            }
+            if (!found) {
+                // not very efficient. refactor to list?
+                SimulatorFile simulatorFile = new SimulatorFile(fileId, shortFileId, content);
+                mFiles = Arrays.copyOf(mFiles, mFiles.length + 1);
+                mFiles[mFiles.length - 1] = simulatorFile;
+            }
+
+        }
+
+        // documentType e.g. "ID"
+        // DocumentType ::= [APPLICATION 1] ICAOString (SIZE (2))
+        public void setDocumentType(String documentType) {
+            ConstructedTlvDataObject constructedTlvDataObject = Asn1DocumentType.getInstance().encode(new TlvTag((byte) 0x61), documentType);
+            String content = HexString.encode(constructedTlvDataObject.toByteArray()).toLowerCase(Locale.ROOT);
+            setFile("0101", "01", content );
+        }
+
+        // GivenNames ::= [APPLICATION 4] UTF8String
+        public void setGivenNames(@NonNull  String givenNames) {
+            ConstructedTlvDataObject constructedTlvDataObject =
+                    Asn1Utf8StringWrapper.getInstance().encode(new TlvTag((byte) 0x64), givenNames);
+            String content = HexString.encode(constructedTlvDataObject.toByteArray()).toLowerCase(Locale.ROOT);
+            setFile("0104", "04", content );
+        }
+
+        // GivenNames ::= [APPLICATION 4] UTF8String
+        public void setFamilyNames(@NonNull  String familyNames) {
+            ConstructedTlvDataObject constructedTlvDataObject =
+                    Asn1Utf8StringWrapper.getInstance().encode(new TlvTag((byte) 0x65), familyNames);
+            String content = HexString.encode(constructedTlvDataObject.toByteArray()).toLowerCase(Locale.ROOT);
+            setFile("0105", "05", content );
+        }
+
+        // NomDePlume ::= [APPLICATION 6] UTF8String
+        public void setNomDePlume(@NonNull  String nomDePlume) {
+            ConstructedTlvDataObject constructedTlvDataObject =
+                    Asn1Utf8StringWrapper.getInstance().encode(new TlvTag((byte) 0x66), nomDePlume);
+            String content = HexString.encode(constructedTlvDataObject.toByteArray()).toLowerCase(Locale.ROOT);
+            setFile("0106", "06", content );
+        }
+
+        // AcademicTitle ::= [APPLICATION 7] UTF8String
+        public void setAcademicTitle(@NonNull  String academicTitle) {
+            ConstructedTlvDataObject constructedTlvDataObject =
+                    Asn1Utf8StringWrapper.getInstance().encode(new TlvTag((byte) 0x67), academicTitle);
+            String content = HexString.encode(constructedTlvDataObject.toByteArray()).toLowerCase(Locale.ROOT);
+            setFile("0107", "07", content );
+        }
+
+        // BirthName ::= [APPLICATION 13] UTF8String
+        public void setBirthName(@NonNull  String birthName) {
+            ConstructedTlvDataObject constructedTlvDataObject =
+                    Asn1Utf8StringWrapper.getInstance().encode(new TlvTag((byte) 0x6d), birthName);
+            String content = HexString.encode(constructedTlvDataObject.toByteArray()).toLowerCase(Locale.ROOT);
+            setFile("010d", "0d", content );
+        }
+
     }
 
     @SerializedName("simulator")
@@ -84,11 +158,39 @@ public class SetCardCommand extends SmartEidCommand {
         mSimulator = new Simulator(files);
     }
 
+    public SetCardCommand(@NonNull String readerName) {
+        this(readerName, new SimulatorFile[]{});
+    }
+
     public String getReaderName() {
         return mReaderName;
     }
 
     public Simulator getSimulator() {
         return mSimulator;
+    }
+
+    public void setFirstNames(@NonNull String firstnames) {
+        mSimulator.setGivenNames(firstnames);
+    }
+
+    public void setBirthName(@NonNull String birthName) {
+        mSimulator.setBirthName(birthName);
+    }
+
+    public void setAcademicTitle(@NonNull String academicTitle) {
+        mSimulator.setAcademicTitle(academicTitle);
+    }
+
+    public void setNomDePlume(@NonNull String nomDePlume) {
+        mSimulator.setNomDePlume(nomDePlume);
+    }
+
+    public void setFamilyNames(@NonNull String familyNames) {
+        mSimulator.setFamilyNames(familyNames);
+    }
+
+    public void setDocumentType(@NonNull String documentType) {
+        mSimulator.setDocumentType(documentType);
     }
 }
