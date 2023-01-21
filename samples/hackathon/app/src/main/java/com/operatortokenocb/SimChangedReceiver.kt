6f180355e5fff110532928efe5810031f81d1735
Louis <louis993546@gmail.com>
2023-01-21 16:10:13 +0100

    Schedule work to be run

--- samples/hackathon/app/src/main/java/com/operatortokenocb/SimChangedReceiver.kt
+++ samples/hackathon/app/src/main/java/com/operatortokenocb/SimChangedReceiver.kt
@@ -3,12 +3,31 @@ package com.operatortokenocb
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import timber.log.Timber
 
 class SimChangedReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         when (val intentAction = intent?.action) {
-            "android.intent.action.SIM_STATE_CHANGED" -> TODO("trigger all the api check")
+            "android.intent.action.SIM_STATE_CHANGED" -> checkWithExistingToken(context ?: error("wtf"))
             else -> error("$intentAction is not expected.")
         }
+    }
+
+    private fun checkWithExistingToken(context: Context) {
+        val workRequest = OneTimeWorkRequestBuilder<TokenCheckingWork>()
+            .setConstraints(
+                Constraints.Builder()
+                    .setRequiredNetworkType(NetworkType.METERED)
+                    .build()
+            )
+            .build()
+
+        WorkManager.getInstance(context).enqueue(workRequest)
+
+        Timber.tag("SCR").d("work scheduled")
     }
 }
